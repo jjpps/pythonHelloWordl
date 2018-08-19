ea68aac0d6231d8ae095393840c562204203fcec
jjpps <joaopedro-ps@hotmail.com>
2018-08-19 01:46:08 -0300

    testando selenium

---
+++ controllChrome.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium
+
+#using chrome
+
+drive = webdriver.Chrome(executable_path="/usr/local/bin/Chromedriver")
+
+driver.get('https://gooogle.com.br')
+
